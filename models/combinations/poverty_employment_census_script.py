0f287d7e68cf310a71b47027d3c44dd541a73498
avasdowney <avasdowney@gmail.com>
2023-01-10 18:29:55 -0500

    ran all model combinations

---
+++ models/combinations/poverty_employment_census_script.py
@@ -0,0 +1,217 @@
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, precision_score, recall_score, accuracy_score, f1_score
+
+import tensorflow as tf
+
+np.random.seed(1)
+
+# %%
+# read data
+df = pd.read_csv('data/all_census_data.csv')
+
+df = df.drop(['Education_Rate'], axis=1)
+
+# %%
+# Labels are the values we want to predict
+labels = np.array(df['SSL SCORE']) # Y
+df = df.drop('SSL SCORE', axis = 1)
+x_list = list(df.columns) # X
+# Convert to numpy array
+df = np.array(df)
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(df, labels, test_size = 0.25, random_state = 42)
+
+#####################################################################################################
+###                                Logistic Regression                                            ###
+#####################################################################################################
+print("Logistic Regression")
+
+# %%
+# instantiate the model (using the default parameters)
+logreg = LogisticRegression(random_state=16)
+
+# fit the model with data
+logreg.fit(X_train, y_train)
+
+y_pred = logreg.predict(X_test)
+
+# %%
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+class_names=[0,1] # name  of classes
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+# create heatmap
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu" ,fmt='g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+
+# %%
+target_names = ['LOW SSL SCORE', 'HIGH SSL SCORE']
+print(classification_report(y_test, y_pred, target_names=target_names))
+
+# %%
+print('Accuracy: %.4f' % accuracy_score(y_test, y_pred))
+print('Precision: %.4f' % precision_score(y_test, y_pred))
+print('Recall: %.4f' % recall_score(y_test, y_pred))
+print('F1 Score: %.4f' % f1_score(y_test, y_pred))
+
+
+#####################################################################################################
+###                                     Random Forest                                             ###
+#####################################################################################################
+print("Random Forest")
+
+rf = RandomForestClassifier(n_estimators = 1000, random_state = 42)
+rf.fit(X_train, y_train);
+y_pred = rf.predict(X_test)
+
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+class_names=[0,1] # name  of classes
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+# create heatmap
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu" ,fmt='g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+
+target_names = ['LOW SSL SCORE', 'HIGH SSL SCORE']
+print(classification_report(y_test, y_pred, target_names=target_names))
+
+print('Accuracy: %.4f' % accuracy_score(y_test, y_pred))
+print('Precision: %.4f' % precision_score(y_test, y_pred))
+print('Recall: %.4f' % recall_score(y_test, y_pred))
+print('F1 Score: %.4f' % f1_score(y_test, y_pred))
+
+#####################################################################################################
+###                                Support Vector Machine                                         ###
+#####################################################################################################
+print("Linear SVM")
+
+clf = svm.SVC(kernel='linear', max_iter=10000) # Linear Kernel
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+class_names=[0,1] # name  of classes
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+# create heatmap
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu" ,fmt='g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+
+target_names = ['LOW SSL SCORE', 'HIGH SSL SCORE']
+print(classification_report(y_test, y_pred, target_names=target_names))
+
+print('Accuracy: %.4f' % accuracy_score(y_test, y_pred))
+print('Precision: %.4f' % precision_score(y_test, y_pred))
+print('Recall: %.4f' % recall_score(y_test, y_pred))
+print('F1 Score: %.4f' % f1_score(y_test, y_pred))
+
+
+
+print("RBF SVM")
+
+clf = svm.SVC(kernel='rbf', max_iter=10000) # Linear Kernel
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+class_names=[0,1] # name  of classes
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+# create heatmap
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu" ,fmt='g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+
+target_names = ['LOW SSL SCORE', 'HIGH SSL SCORE']
+print(classification_report(y_test, y_pred, target_names=target_names))
+
+print('Accuracy: %.4f' % accuracy_score(y_test, y_pred))
+print('Precision: %.4f' % precision_score(y_test, y_pred))
+print('Recall: %.4f' % recall_score(y_test, y_pred))
+print('F1 Score: %.4f' % f1_score(y_test, y_pred))
+
+#####################################################################################################
+###                                      Neural Network                                           ###
+#####################################################################################################
+print("Neural Network")
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+
+predictions = model(X_train[:1]).numpy()
+predictions
+
+tf.nn.softmax(predictions).numpy()
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+loss_fn(y_train[:1], predictions).numpy()
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=5)
+
+y_pred = model.predict(X_test)
+y_pred = np.argmax(y_pred, axis=1)
+
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+class_names=[0,1] # name  of classes
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+# create heatmap
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu" ,fmt='g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+
+target_names = ['LOW SSL SCORE', 'HIGH SSL SCORE']
+print(classification_report(y_test, y_pred, target_names=target_names))
+
+print('Accuracy: %.4f' % accuracy_score(y_test, y_pred))
+print('Precision: %.4f' % precision_score(y_test, y_pred))
+print('Recall: %.4f' % recall_score(y_test, y_pred))
+print('F1 Score: %.4f' % f1_score(y_test, y_pred))
